75ba8dedc00add100a7e2891533f3966f4aef5bc
Tony Barbour <tony@LunarG.com>
2017-01-31 15:24:00 -0700

    layers: Move cmd collection from validate to record
    Change-Id: I89df7358f1916ea4688d4b8cfe2cb870f946b3d9

--- layers/core_validation.cpp
+++ layers/core_validation.cpp
@@ -4574,7 +4574,7 @@ static bool ValidateFenceForSubmit(layer_data *dev_data, FENCE_NODE *pFence) {
 }
 
 static void PostCallRecordQueueSubmit(layer_data *dev_data, VkQueue queue, uint32_t submitCount, const VkSubmitInfo *pSubmits,
-                                      VkFence fence, std::vector<VkCommandBuffer> *cbs) {
+                                      VkFence fence) {
     auto pQueue = getQueueState(dev_data, queue);
     auto pFence = getFenceNode(dev_data, fence);
 
@@ -4585,6 +4585,7 @@ static void PostCallRecordQueueSubmit(layer_data *dev_data, VkQueue queue, uint3
 
     // Now process each individual submit
     for (uint32_t submit_idx = 0; submit_idx < submitCount; submit_idx++) {
+        std::vector<VkCommandBuffer> cbs;
         const VkSubmitInfo *submit = &pSubmits[submit_idx];
         vector<SEMAPHORE_WAIT> semaphore_waits;
         vector<VkSemaphore> semaphore_signals;
@@ -4614,6 +4615,10 @@ static void PostCallRecordQueueSubmit(layer_data *dev_data, VkQueue queue, uint3
         for (uint32_t i = 0; i < submit->commandBufferCount; i++) {
             auto cb_node = getCBNode(dev_data, submit->pCommandBuffers[i]);
             if (cb_node) {
+                cbs.push_back(submit->pCommandBuffers[i]);
+                for (auto secondaryCmdBuffer : cb_node->secondaryCommandBuffers) {
+                    cbs.push_back(secondaryCmdBuffer);
+                }
                 UpdateCmdBufImageLayouts(dev_data, cb_node);
                 incrementResources(dev_data, cb_node);
                 if (!cb_node->secondaryCommandBuffers.empty()) {
@@ -4624,7 +4629,7 @@ static void PostCallRecordQueueSubmit(layer_data *dev_data, VkQueue queue, uint3
                 }
             }
         }
-        pQueue->submissions.emplace_back(*cbs, semaphore_waits, semaphore_signals,
+        pQueue->submissions.emplace_back(cbs, semaphore_waits, semaphore_signals,
                                          submit_idx == submitCount - 1 ? fence : VK_NULL_HANDLE);
     }
 
@@ -4638,7 +4643,7 @@ static void PostCallRecordQueueSubmit(layer_data *dev_data, VkQueue queue, uint3
 }
 
 static bool PreCallValidateQueueSubmit(layer_data *dev_data, VkQueue queue, uint32_t submitCount, const VkSubmitInfo *pSubmits,
-                                       VkFence fence, std::vector<VkCommandBuffer> *cbs) {
+                                       VkFence fence) {
     auto pFence = getFenceNode(dev_data, fence);
     bool skip_call = ValidateFenceForSubmit(dev_data, pFence);
     if (skip_call) {
@@ -4694,11 +4699,6 @@ static bool PreCallValidateQueueSubmit(layer_data *dev_data, VkQueue queue, uint
             auto cb_node = getCBNode(dev_data, submit->pCommandBuffers[i]);
             skip_call |= ValidateCmdBufImageLayouts(dev_data, cb_node, localImageLayoutMap);
             if (cb_node) {
-                cbs->push_back(submit->pCommandBuffers[i]);
-                for (auto secondaryCmdBuffer : cb_node->secondaryCommandBuffers) {
-                    cbs->push_back(secondaryCmdBuffer);
-                }
-
                 current_cmds.insert(submit->pCommandBuffers[i]);
                 skip_call |= validatePrimaryCommandBufferState(dev_data, cb_node, (int) current_cmds.count(submit->pCommandBuffers[i]));
                 skip_call |= validateQueueFamilyIndices(dev_data, cb_node, queue);
@@ -4727,10 +4727,9 @@ static bool PreCallValidateQueueSubmit(layer_data *dev_data, VkQueue queue, uint
 
 VKAPI_ATTR VkResult VKAPI_CALL QueueSubmit(VkQueue queue, uint32_t submitCount, const VkSubmitInfo *pSubmits, VkFence fence) {
     layer_data *dev_data = get_my_data_ptr(get_dispatch_key(queue), layer_data_map);
-    std::vector<VkCommandBuffer> cbs;
     std::unique_lock<std::mutex> lock(global_lock);
 
-    bool skip = PreCallValidateQueueSubmit(dev_data, queue, submitCount, pSubmits, fence, &cbs);
+    bool skip = PreCallValidateQueueSubmit(dev_data, queue, submitCount, pSubmits, fence);
     lock.unlock();
 
     if (skip)
@@ -4739,7 +4738,7 @@ VKAPI_ATTR VkResult VKAPI_CALL QueueSubmit(VkQueue queue, uint32_t submitCount, 
     VkResult result = dev_data->dispatch_table.QueueSubmit(queue, submitCount, pSubmits, fence);
 
     lock.lock();
-    PostCallRecordQueueSubmit(dev_data, queue, submitCount, pSubmits, fence, &cbs);
+    PostCallRecordQueueSubmit(dev_data, queue, submitCount, pSubmits, fence);
     lock.unlock();
     return result;
 }
